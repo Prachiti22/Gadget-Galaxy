0a90f750e92fac348b7a465f34f16268d7266d32
Prachiti22 <97396979+Prachiti22@users.noreply.github.com>
2025-10-30 16:38:30 -0400

    Add files via upload

---
+++ Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2023 Gadget Galaxy All rights reserverd.</p>
+      <p className="icons">
+
+      </p>
+    </div>
+  )
+}
+
+export default Footer
